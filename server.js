1b863cddd7986ccf2378da90a3629eb646af18c8
Abraham Aguirre <aguirre.abraham@gmail.com>
2022-03-20 00:07:26 -0400

    refractor code

--- server.js
+++ server.js
@@ -1,15 +1,75 @@
 const express = require('express');
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 const PORT = process.env.PORT || 3001;
-const sqlHost ='172.24.208.1';
 const app = express();
+
+const db = mysql.createConnection(
+    {
+      host: '172.24.208.1',
+      // Your MySQL username,
+      user: 'wsl',
+      // Your MySQL password
+      password: 'password',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
     });
   });
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
   });
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  }); 
+// Create a candidate
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  });
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
